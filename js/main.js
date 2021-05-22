958d17ecd804d974689d50f8daacf3b420fd8705
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-05-22 19:24:39 +0300

    сделано ДЗ

--- js/main.js
+++ js/main.js
@@ -1,47 +1,78 @@
 
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2019'),
-//   });
-
 const refs = {
     days: document.querySelector('[data-value="days"]'),
     hours: document.querySelector('[data-value="hours"]'),
-    min: document.querySelector('[data-value="mins"]'),
+    mins: document.querySelector('[data-value="mins"]'),
     secs: document.querySelector('[data-value="secs"]'),
 }
 
-const timer = {
+//! Делаем класс 
+
+class CountdownTimer{
+    static description = 'Логика счетчика'
+    constructor({selector, targetDate}){
+        this.selector = selector;
+        this.targetDate = targetDate;
+    }
+   
     start(){
-        const targetDate = new Date('May 30, 2021');// День Киева
-        setInterval(()=>{
+            const dataID = setInterval(()=>{
             const currentTime = Date.now();
-            const deltaTime = (targetDate-currentTime);
-            const {days, hours, mins, secs} = getTimerComponents(deltaTime)
-            // console.log(`${days}:${hours}:${mins}:${secs}`)
-            refs.days.textContent = `${days}`;
-            refs.hours.textContent = `${hours}`;
-            refs.min.textContent = `${mins}`;
-            refs.secs.textContent = `${secs}`
+            const deltaTime = (this.targetDate-currentTime);
+            const finalTime = this.getTimerComponents(deltaTime);
+            this.updateInterface(finalTime);
+            this.stop(finalTime,dataID)
         },1000)
+    }
+
+    stop({days, hours, mins, secs},id){
+        if(days === `00` &&
+        hours === `00`&&
+        mins === `00`&&
+        secs === `00`){
+           clearInterval(id)
+           alert('Finish') 
+        }
+    }
+    
+    updateInterface({days, hours, mins, secs}){
+        refs.days.textContent = `${days}`;
+        refs.hours.textContent = `${hours}`;
+        refs.mins.textContent = `${mins}`;
+        refs.secs.textContent = `${secs}`;
+    }
+
+    pad(value){
+        return String(value).padStart(2,'0');
+    }
+
+    getTimerComponents(time){
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        return {days, hours, mins, secs};
     }
 }
 
-timer.start()
+const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('May 30, 2021'),
+});
 
-//! Преобразовует число в строку и дописывает слева, до 2-х символов 0.
-//! 1=01 2=02 12=12
-function pad(value){
-    return String(value).padStart(2,'0');
-}
+timer.start();
 
-//! Берет UNIX время и проверяте сколко есть дней-часов-минут-секунд
-function getTimerComponents(time){
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-    return {days, hours, mins, secs};
-}
+// //! Преобразовует число в строку и дописывает слева, до 2-х символов 0.
+// //! 1=01 2=02 12=12
+// function pad(value){
+//     return String(value).padStart(2,'0');
+// }
 
-
+// //! Берет UNIX время и проверяте сколко есть дней-часов-минут-секунд
+// function getTimerComponents(time){
+//     const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//     const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+//     return {days, hours, mins, secs};
+// }
